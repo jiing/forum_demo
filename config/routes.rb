988726501a6db0976a60cc92f37382c47118446a
Jiing Deng <jiing@jiing.org>
2011-07-21 15:06:26 +0800

    complete rails101 assignment 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 ForumDemo::Application.routes.draw do
 
+  resources :boards do
+    resources :posts
+  end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +53,7 @@ ForumDemo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "pages#welcome"
+  root :to => "boards#index"
 
   # See how all your routes lay out with "rake routes"
 
